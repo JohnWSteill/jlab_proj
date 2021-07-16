0eb9d83b088b54d5ee1091940a76d94ced39aab3
John Steill <jsteill@morgridge.org>
2021-07-16 12:37:55 -0500

    added precommit hook to clear nb cells

--- somite_study_library.py
+++ somite_study_library.py
@@ -181,3 +181,20 @@ def plot_targetmiRNA_group(miRNA,C):
             y = miRNA.X[:,i]
             ax.plot(time,y/np.linalg.norm(y), label=miRNA.var.index[i].split(',')[0])
             ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            
+def get_normed_and_zero_filtered_data(filenamei, alg = 'cpm', quantile=None):
+    df = pd.read_csv(filename, sep = '\t', index_col = 0)
+    df = df.loc[df.sum(axis=1) != 0,:]
+    assert alg in ['cpm', 'qbr']
+    if alg == 'cpm':
+         df = df / df.mean() * 1e6
+    if alg == 'qbr':
+        quantile = int(quantile)
+        assert quantile > 0 and quantile < 100
+      
+    df = (df
+          .assign(sum=df.sum(axis=1))
+          .sort_values(by='sum', ascending=False)
+          .drop('sum', axis = 1)
+         )
+    return df
